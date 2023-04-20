ac25c6b733bb19083f718f75944d5fd189353292
Franco Damilano <francodemilano@gmail.com>
2023-04-19 21:12:29 -0300

    changes in app

--- EntregaFinal/src/app.js
+++ EntregaFinal/src/app.js
@@ -1,20 +1,40 @@
-import  express  from "express";
+import express from "express";
 import productsRouter from "./routes/products.router.js"
 import cartsRouter from "./routes/carts.router.js";
+import viewsRouter from "./routes/views.router.js"
 import __dirname from "./utils.js";
-
+import handlebars from "express-handlebars"
+import { Server } from "socket.io";
+import fs from 'fs'
 
 const app = express();
 const PORT = 8080;
 
-
-app.listen(PORT, ()=>{
-    console.log(`Servidor funcionando en el puerto ${PORT}`)
+const appServer = app.listen(PORT, () => {
+    console.log(`Server started on ${PORT} ports.`)
 })
 
+export const socketServer = new Server(appServer)
+
+socketServer.on('connection', (socket) => {
+    console.log('Usuario encontrado', socket.id)
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado')
+    })
+
+})
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'))
 
+
+app.use('/api/products', productsRouter)
 app.use('/api/carts', cartsRouter)
-app.use('/api/products',productsRouter)
+app.use('/', viewsRouter)
--- EntregaFinal/src/utils.js
+++ EntregaFinal/src/utils.js
@@ -5,7 +5,6 @@ import multer from 'multer';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, __dirname + '/public/images')
@@ -16,6 +15,5 @@ const storage = multer.diskStorage({
 })
 
 export const uploaderThumbnails = multer({ storage })
-
 
 export default __dirname;
