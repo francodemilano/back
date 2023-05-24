47f09e421555d04c6a2cbb0aedf554cc58c540ea
Franco Damilano <francodemilano@gmail.com>
2023-05-24 20:55:22 -0300

    pmjs

--- EntregaFinal/src/Dao/controllers/ProductManagerMongo.js
+++ EntregaFinal/src/Dao/controllers/ProductManagerMongo.js
@@ -1,15 +1,6 @@
 import productModel from "../models/products.model.js";
 
 export default class ProductManagerMongo {
-  async getProducts() {
-    const result = await productModel.find();
-    return result;
-  }
-
-  async getProductsById(id) {
-    const result = await productModel.find({ _id: id });
-    return result;
-  }
 
   async addProduct(productData) {
     const { title, description, price, category, status, img, code, stock } = productData;
@@ -21,14 +12,4 @@ export default class ProductManagerMongo {
     return result;
   }
 
-  async deleteProduct(id) {
-    const result = await productModel.deleteOne({ _id: id });
-    return result;
-  }
-
-  async updateProduct(id, updateProduct) {
-    const result = await productModel.updateOne({ _id: id }, { $set: updateProduct });
-    return result;
-  }
 }
-
